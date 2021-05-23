2c2d13232d037604108272c909ae711f59f01304
tahabinimran <74494109+tahabinimran@users.noreply.github.com>
2021-05-23 15:47:17 +0500

    MetaProteins

---
+++ Metaprotein/Decision Tree Codes/OVGU_C4.5_Decision_Tree/src/C45CoreAlgorithm/JUnitTesting.java
@@ -0,0 +1,65 @@
+package C45CoreAlgorithm;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import DataDefination.Attribute;
+import DataDefination.Instance;
+
+
+public class JUnitTesting {
+	ArrayList<Attribute> attributeSet;
+	ArrayList<Instance> instanceSet;
+	public Attribute targetAttribute;
+
+	
+	public int square(int x) {
+		return x*x;
+	}
+	
+	public void addGolfAttributeSet() throws IOException {
+		Attribute attr1 = new Attribute("Outlook","{Sunny,Overcast,Rain}");
+		attributeSet.add(attr1);
+		Attribute attr2 = new Attribute("Temperature","real");
+		attributeSet.add(attr2);
+		Attribute attr3 = new Attribute("Humidity","real");
+		attributeSet.add(attr3);
+		Attribute attr4 = new Attribute("Wind","{Weak,Strong}");
+		attributeSet.add(attr4);
+		Attribute attr5 = new Attribute("Decision","{Yes,No}");
+		attributeSet.add(attr5);
+	}
+	
+	
+	public void ProcessInputData(String fileName) throws IOException {
+		attributeSet = new ArrayList<Attribute>();
+		instanceSet = new ArrayList<Instance>();
+
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(new File(fileName));
+
+		addGolfAttributeSet();
+
+		targetAttribute = attributeSet.get(attributeSet.size() - 1);
+
+
+		// Put all instances into instanceSet
+		while (in.hasNextLine()) {
+			String line = in.nextLine();
+			String[] lineArr = line.split(",");
+			Instance item = new Instance();
+			for (int i = 0; i < lineArr.length; i++) {
+				item.addAttribute(attributeSet.get(i).getName(), lineArr[i]);
+			}
+			instanceSet.add(item);
+		}
+		
+		in.close();
+		
+	}
+	
+	
+	
+}
---
+++ Metaprotein/Decision Tree Codes/OVGU_C4.5_Decision_Tree/src/ClassifyMain.java
@@ -0,0 +1,29 @@
+/**
+ * Main class to build the decision tree and test on the test dataset
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+import MineData.C45MineData;
+import ProcessOutput.PrintTree;
+
+public class ClassifyMain {
+	public static void main(String[] args) throws IOException {		
+		Scanner in = new Scanner(System.in);
+		long startTime = System.nanoTime();
+		
+		C45MineData mine = new C45MineData("data/train.csv", "data/test.csv");
+		
+		mine.calculateAccuracy();
+		
+		long endTime = System.nanoTime();
+	    long elapsedTime = endTime - startTime;
+	    double msTime = (double) elapsedTime / 1000000.0;
+	    System.out.println("Time taken: " + msTime + " ms\n");
+		
+	    PrintTree tree = new PrintTree();
+	    System.out.println(tree.printDFS(mine.getRoot()));
+	    
+		in.close();
+	}
+}
