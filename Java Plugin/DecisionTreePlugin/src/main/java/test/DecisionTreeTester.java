59863a02e99a1b1eb65ffab264b7df4090ef302e
nasim-ahmed <nasim.ahmed54@gmail.com>
2021-08-06 01:43:10 +0200

    added junit test

--- Java Plugin/DecisionTreePlugin/src/main/java/test/DecisionTreeTester.java
+++ Java Plugin/DecisionTreePlugin/src/main/java/test/DecisionTreeTester.java
@@ -1,37 +1,31 @@
-package unittesting;
+package test;
 
+import static org.junit.jupiter.api.Assertions.*;
 import core.ChooseAttribute;
 import definition.Attribute;
 import definition.Instance;
-import evaluate.EvaluateTree;
 import input.ProcessInputData;
 import node.TreeNode;
 import core.ConstructTree;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-
-import static org.junit.Assert.*;
-import org.junit.Test;
 
 
-public class PractiseTest {
-	public static final String filePath = "C:/Users/49171/Desktop/Graph-Database-Learning-Algorithms-Neo4j-/"
-			+ "Heart Failure Prediction/Decision Tree Codes/OVGU_C4.5_Decision_Tree/data/junit.csv";
-	public static final String filePath1 = "C:/Users/49171/Desktop/Graph-Database-Learning-Algorithms-Neo4j-/"
-			+ "Heart Failure Prediction/Decision Tree Codes/OVGU_C4.5_Decision_Tree/data/junit_train.csv";
-	public static final String filePath2 = "C:/Users/49171/Desktop/Graph-Database-Learning-Algorithms-Neo4j-/"
-			+ "Heart Failure Prediction/Decision Tree Codes/OVGU_C4.5_Decision_Tree/data/junit_test.csv";
+import org.junit.jupiter.api.Test;
+
+class DecisionTreeTester {
+	public static final String filePath = "data/junit.csv";
+	public static final String filePath1 = "data/junit_train.csv";
+	public static final String filePath2 = "data/junit_test.csv";
 	
 	public ArrayList<Attribute> attributes = new ArrayList<Attribute>();
 	public ArrayList<Instance> instances = new ArrayList<Instance>();
 	public Attribute targets = new Attribute();
 	
-	
 	@Test
 	public void testProcessInputData() throws IOException {
-				 
+		 
 		ProcessInputData input = new ProcessInputData(filePath);
 		attributes = input.getAttributeSet();
 		instances = input.getInstanceSet();
@@ -42,11 +36,8 @@ public class PractiseTest {
 		assertEquals( "var_2" , attributes.get(2).getName());
 		assertEquals( "var_3" , attributes.get(3).getName());
 		
-		assertEquals( 8 , instances.size());
-		 		
+		assertEquals(8,instances.size()); 		
 	}
-	
-
 	
 	@Test
 	public void testChooseAttribute() throws IOException {
@@ -74,20 +65,10 @@ public class PractiseTest {
 		System.out.println(nodes);	
 
 		assertEquals("id", nodes.getAttribute().getName());
-
 	}
 	 
 	
 	
-	@Test
-	public void testAccuracy() throws IOException {
-
-		EvaluateTree data = new EvaluateTree(filePath1,filePath2);
-		double accuracy = data.calculateAccuracy();
-		System.out.println(accuracy);
-
-		assertEquals( 50, accuracy, 0.1);
-
-	}
+	
 
 }
