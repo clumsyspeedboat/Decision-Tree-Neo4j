57da612bff1f86aa1bc3a6c56ece9b1510ae8be4
Rahul Mondal <57228346+clumsyspeedboat@users.noreply.github.com>
2021-09-15 22:47:54 +0200

    Create dt_analysis.R

---
+++ DT Analysis/dt_analysis.R
@@ -0,0 +1,54 @@
+#########################
+###### DT_ANALYSIS ######
+#########################
+
+# Cleaning the work space #
+cat("\f")       # Clear old outputs
+rm(list=ls())   # Clear all variables
+
+# Creating a data matrix #
+data <- file.choose()
+data_matrix <- read.csv(data, header = TRUE, sep = ",", na.strings=c("","NA"))
+
+# Data Subsetting #
+dt_r <- data_matrix[which(data_matrix$language == "r"),]
+dt_python <- data_matrix[which(data_matrix$language == "python"),]
+dt_java <- data_matrix[which(data_matrix$language == "java"),]
+dt_neo_csv <- data_matrix[which(data_matrix$language == "neo4j_csv"),]
+dt_neo_db <- data_matrix[which(data_matrix$language == "neo4j_db"),]
+
+
+# Accuracy #
+r_gini <- mean(dt_r$accuracy[which(dt_r$splitting_criterion == "gini")])
+r_gainratio <- mean(dt_r$accuracy[which(dt_r$splitting_criterion == "gain ratio")])
+
+python_gini <- mean(dt_python$accuracy[which(dt_python$splitting_criterion == "gini")])
+python_infogain <- mean(dt_python$accuracy[which(dt_python$splitting_criterion == "info gain")])
+
+java_gini <- mean(dt_java$accuracy[which(dt_java$splitting_criterion == "gini")])
+java_infogain <- mean(dt_java$accuracy[which(dt_java$splitting_criterion == "info gain")])
+java_gainratio <- mean(dt_java$accuracy[which(dt_java$splitting_criterion == "gain ratio")])
+
+neo4j_csv_gini <- mean(dt_neo_csv$accuracy[which(dt_neo_csv$splitting_criterion == "gini")])
+neo4j_csv_infogain <- mean(dt_neo_csv$accuracy[which(dt_neo_csv$splitting_criterion == "info gain")])
+neo4j_csv_gainratio <- mean(dt_neo_csv$accuracy[which(dt_neo_csv$splitting_criterion == "gain ratio")])
+
+neo4j_db_gini <- mean(dt_neo_db$accuracy[which(dt_neo_db$splitting_criterion == "gini")])
+neo4j_db_infogain <- mean(dt_neo_db$accuracy[which(dt_neo_db$splitting_criterion == "info gain")])
+neo4j_db_gainratio <- mean(dt_neo_db$accuracy[which(dt_neo_db$splitting_criterion == "gain ratio")])
+
+
+# Generation Time #
+r_gini <- mean(dt_r$generation_time[which(dt_r$splitting_criterion == "gini")])
+r_gainratio <- mean(dt_r$generation_time[which(dt_r$splitting_criterion == "gain ratio")])
+
+python_gini <- mean(dt_python$generation_time[which(dt_python$splitting_criterion == "gini")])
+python_infogain <- mean(dt_python$generation_time[which(dt_python$splitting_criterion == "info gain")])
+
+java_gini <- mean(dt_java$generation_time[which(dt_java$splitting_criterion == "gini")])
+java_infogain <- mean(dt_java$generation_time[which(dt_java$splitting_criterion == "info gain")])
+java_gainratio <- mean(dt_java$generation_time[which(dt_java$splitting_criterion == "gain ratio")])
+
+neo4j_csv_gini <- mean(dt_neo_csv$generation_time[which(dt_neo_csv$splitting_criterion == "gini")])
+neo4j_csv_infogain <- mean(dt_neo_csv$generation_time[which(dt_neo_csv$splitting_criterion == "info gain")])
+neo4j_csv_gainratio <- mean(dt_neo_csv$generation_time[which(dt_neo_csv$splitting_criterion == "gain ratio")])
